9163d785bf3a020fb7b9229c80880a03c9fe3410
zemix30004 <eugenezm@gmail.com>
2021-09-12 18:45:21 +0300

    products migrate and seeder

---
+++ database/migrations/2021_09_12_152710_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->string('name');
+            $table->string('code');
+            $table->text('description')->nullable();
+            $table->text('image')->nullable();
+            $table->decimal('price')->default(0);
+            $table->decimal('size');
+            $table->boolean('is_spicy');
+            $table->boolean('is_veg');
+            $table->boolean('is_best_offer');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -27,119 +27,133 @@ Pizza-Shop
           </div>
         </div>
         <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text></svg>
+			<div class="card shadow-sm">
+				 <img src="/images/products/Гавайская пицца.jpg" alt="Гавайская пицца"  width="100%" height="225"class="card shadow-sm">
+			  {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="115" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text>
+			  </svg> --}}
 
-              <div class="card-body">
-                <p class="card-text">Королевская пицца</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text></svg>
+			  <div class="card-body">
+				 <p class="card-text">Королевская пицца</p>
+				 <div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+					  <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
+					  <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
+					</div>
+					<small class="text-muted">120 uah</small>
+				 </div>
+			  </div>
+			</div>
+		 </div>
+		 <div class="col">
+			<div class="card shadow-sm">
+				 <img src="/images/products/Гавайская пицца.jpg" alt="Гавайская пицца"  width="100%" height="225"class="card shadow-sm">
+			  {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="115" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text>
+			  </svg> --}}
 
-              <div class="card-body">
-                <p class="card-text">Королевская пицца</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
+			  <div class="card-body">
+				 <p class="card-text">Королевская пицца</p>
+				 <div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+					  <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
+					  <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
+					</div>
+					<small class="text-muted">120 uah</small>
+				 </div>
+			  </div>
+			</div>
+		 </div>
 
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text></svg>
+		 <div class="col">
+			<div class="card shadow-sm">
+				 <img src="/images/products/Гавайская пицца.jpg" alt="Гавайская пицца"  width="100%" height="225"class="card shadow-sm">
+			  {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="115" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text>
+			  </svg> --}}
 
-              <div class="card-body">
-                <p class="card-text">Королевская пицца</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text></svg>
+			  <div class="card-body">
+				 <p class="card-text">Королевская пицца</p>
+				 <div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+					  <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
+					  <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
+					</div>
+					<small class="text-muted">120 uah</small>
+				 </div>
+			  </div>
+			</div>
+		 </div>
+		 <div class="col">
+			<div class="card shadow-sm">
+				 <img src="/images/products/Гавайская пицца.jpg" alt="Гавайская пицца"  width="100%" height="225"class="card shadow-sm">
+			  {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="115" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text>
+			  </svg> --}}
 
-              <div class="card-body">
-                <p class="card-text">Королевская пицца</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text></svg>
+			  <div class="card-body">
+				 <p class="card-text">Королевская пицца</p>
+				 <div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+					  <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
+					  <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
+					</div>
+					<small class="text-muted">120 uah</small>
+				 </div>
+			  </div>
+			</div>
+		 </div>
+		 <div class="col">
+			<div class="card shadow-sm">
+				 <img src="/images/products/Гавайская пицца.jpg" alt="Гавайская пицца"  width="100%" height="225"class="card shadow-sm">
+			  {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="115" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text>
+			  </svg> --}}
 
-              <div class="card-body">
-                <p class="card-text">Королевская пицца</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
+			  <div class="card-body">
+				 <p class="card-text">Королевская пицца</p>
+				 <div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+					  <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
+					  <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
+					</div>
+					<small class="text-muted">120 uah</small>
+				 </div>
+			  </div>
+			</div>
+		 </div>
 
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text></svg>
+		 <div class="col">
+			<div class="card shadow-sm">
+				 <img src="/images/products/Гавайская пицца.jpg" alt="Гавайская пицца"  width="100%" height="225"class="card shadow-sm">
+			  {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="115" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text>
+			  </svg> --}}
 
-              <div class="card-body">
-                <p class="card-text">Королевская пицца</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text></svg>
+			  <div class="card-body">
+				 <p class="card-text">Королевская пицца</p>
+				 <div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+					  <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
+					  <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
+					</div>
+					<small class="text-muted">120 uah</small>
+				 </div>
+			  </div>
+			</div>
+		 </div>
+		 <div class="col">
+			<div class="card shadow-sm">
+				 <img src="/images/products/Гавайская пицца.jpg" alt="Гавайская пицца"  width="100%" height="225"class="card shadow-sm">
+			  {{-- <svg class="bd-placeholder-img card-img-top" width="100%" height="115" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text>
+			  </svg> --}}
 
-              <div class="card-body">
-                <p class="card-text">Королевская пицца</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
-                </div>
-              </div>
-            </div>
-          </div>
+			  <div class="card-body">
+				 <p class="card-text">Королевская пицца</p>
+				 <div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+					  <button type="button" class="btn btn-sm btn-outline-secondary">В корзину</button>
+					  <button type="button" class="btn btn-sm btn-outline-secondary">Подробнее</button>
+					</div>
+					<small class="text-muted">120 uah</small>
+				 </div>
+			  </div>
+			</div>
+		 </div>
         <div class="col">
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Описание</text></svg>
